a0fe7691867adc967d040e68685e05bbf4d8f2ec
umutbarandemir <baran292907@hotmail.com>
2025-01-12 21:58:22 +0300

    Blog project base

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("blogimage").addEventListener("click", function () {
+    // Redirect to another page
+    window.location.href = "blogpage.html"; // Replace "anotherPage.html" with your target page's URL
+});
